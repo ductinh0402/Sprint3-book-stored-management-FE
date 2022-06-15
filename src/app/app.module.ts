1794441182ac49a35d3b63d661a582646d09331e
ductinh0402 <hoangductinh0402@gmail.com>
2022-06-15 14:35:50 +0700

    tinh push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
